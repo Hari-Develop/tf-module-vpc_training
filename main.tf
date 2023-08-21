4d1b939222166e6b9343b4955cf149e88f389ff0
harinadhareddy <yadalaharinadhareddy01@gmail.com>
2023-08-21 21:22:14 +0530

    file changes

--- main.tf
+++ main.tf
@@ -2,5 +2,6 @@ module "vpc" {
   source = "git::https://github.com/Hari-Develop/training_1"
   for_each = var.vpc
   env = var.env
+  subnets = each.value["subnets"]
   cidr_block = each.value["cidr_block"]
 }
